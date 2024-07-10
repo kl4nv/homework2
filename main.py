7be3e90946271039126b3fec471596243d8694ef
kl4nv <kupull94@bk.ru>
2024-07-10 14:47:52 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+HW = 12
+time = 1.5
+name = "Python"
+HW1 = str(HW)
+time1 = str(time)
+print(name+',','всего задач:',HW1+",",'затрачено часов:',time1+',','среднее время выполнения:',time/HW,'часа')
