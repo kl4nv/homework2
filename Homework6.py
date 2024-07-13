95a668aa86093905cb8ca2f27b86a493c8845005
kl4nv <kupull94@bk.ru>
2024-07-13 17:03:48 +1000

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,8 @@
+my_dict = {'Kirill': 12345, 'Mark':777777, "Lena":565656}
+print(my_dict)
+print(my_dict['Kirill'])
+print(my_dict.get('vvv'))
+my_dict['Bob'] = 65983
+my_dict['Karl'] = 239856
+del my_dict['Mark']
+print(my_dict)
