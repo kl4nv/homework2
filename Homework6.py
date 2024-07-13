ca6538a3fccfa65927b55bbf69e75e11cbe68a1d
kl4nv <kupull94@bk.ru>
2024-07-13 17:05:07 +1000

    Homework6.py

--- Homework6.py
+++ Homework6.py
@@ -6,3 +6,9 @@ my_dict['Bob'] = 65983
 my_dict['Karl'] = 239856
 del my_dict['Mark']
 print(my_dict)
+my_set = {8, 2.5, 'kkk', 8, True, 2, 2.5}
+print(my_set)
+my_set.update({55,9})
+print(my_set)
+my_set.discard(True)
+print(my_set)
