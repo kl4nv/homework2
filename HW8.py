e7c03254357342cb25bf50def7a41fc8296d3b25
kl4nv <kupull94@bk.ru>
2024-07-19 20:51:25 +1000

    Update HW8.py

--- HW8.py
+++ HW8.py
@@ -1,8 +1,7 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
-while a <= (len(my_list) - 1) and my_list[a] >= 0:
-        print(my_list[a])
+while a < len(my_list) and my_list[a] >= 0:
+    if my_list[a] == 0:
         a += 1
-
-
-
+    print(my_list[a])
+    a += 1
