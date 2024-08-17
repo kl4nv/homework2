6fe90b5dbdd8765360e9c126dcac38de1b66a9ed
kl4nv <kupull94@bk.ru>
2024-08-17 13:11:14 +1000

    Add files via upload

--- module 4.2.py
+++ module 4.2.py
@@ -1,8 +1,17 @@
-def test_function():
-    def inner_function():
-        print('я в области видимости функции test_function')
-    return inner_function()
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
 
-test_function()
-inner_function() # Выдает ошибку, потому что функция находится не в глобальном пространстве имен
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            return print('Такого этажа нет')
 
+
+info = House('ЖК Полюса', 25)
+info.go_to(3)
+info1 = House('ЖК Космос', 45)
+info1.go_to(50)
