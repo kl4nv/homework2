c3758956ce0bd32604a3f7dcfe8a6c69f83836d0
kl4nv <kupull94@bk.ru>
2024-08-29 09:52:17 +1000

    Add files via upload

---
+++ module 6.3.py
@@ -0,0 +1,40 @@
+class Horse:
+    x_distance = 0
+
+    def __init__(self):
+        self.sound = 'Frrr'
+        super().__init__()
+
+    def run(self, dx):
+        self.x_distance += dx
+        return self.x_distance
+
+class Eagle:
+    y_distance = 0
+
+    def __init__(self):
+        self.sound = 'I train, eat, sleep, and repeat'
+
+    def fly(self, dy):
+        self.y_distance += dy
+        return self.y_distance
+
+class Pegasus(Horse, Eagle):
+
+
+    def move(self, dx, dy):
+        return self.run(dx), self.fly(dy)
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+    def voice(self):
+       print(self.sound)
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
